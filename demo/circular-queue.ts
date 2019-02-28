58e4fcd52887d288298319b90c2244df16f638fd
lilongji <woshililongji@163.com>
2019-02-28 18:03:04 +0800

    update 2019-02-23-数据结构-1.线性结构

---
+++ demo/circular-queue.ts
@@ -0,0 +1,46 @@
+class CircularQueue {
+  queue: any[];
+  head: number = 0;
+  tail: number = 0;
+  size: number = 0;
+
+  constructor(length: number) {
+    this.queue = new Array(length);
+  }
+
+  enQueue(value: any): boolean {
+    if (this.isFull()) return false;
+    if (!this.isEmpty()) {
+      if (this.tail === this.queue.length - 1) this.tail = 0;
+      else this.tail ++;
+    }
+    this.queue[this.tail] = value;
+    this.size ++;
+    return true;
+  }
+
+  deQueue(): boolean {
+    if (this.isEmpty()) return false;
+    this.queue[this.head] = null;
+    this.size --;
+    if (this.head === this.queue.length - 1) this.head = 0;
+    else if (this.head !== this.tail) this.head ++;
+    return true;
+  }
+
+  Front(): any {
+    return this.isEmpty() ? -1 : this.queue[this.head];
+  }
+
+  Rear(): any {
+    return this.isEmpty() ? -1 : this.queue[this.tail];
+  }
+
+  isEmpty(): boolean {
+    return !this.size;
+  }
+
+  isFull(): boolean {
+    return this.size === this.queue.length;
+  }
+}
