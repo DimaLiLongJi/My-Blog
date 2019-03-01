04b21533a8cbd161ae5a93bf1976d4e14f7930ae
lilongji <woshililongji@163.com>
2019-03-01 18:42:55 +0800

    add 2019-02-27-数据结构-2. and 队列与广度优先搜

--- demo/circular-queue.ts
+++ demo/circular-queue.ts
@@ -1,5 +1,55 @@
 class CircularQueue {
   queue: any[];
+  head: number = -1;
+  tail: number = -1;
+  size: number = 0;
+
+  constructor(length: number) {
+    this.queue = new Array(length);
+  }
+
+  enQueue(value: any): boolean {
+    if (this.isFull()) return false;
+    if (this.isEmpty()) this.head = 0;
+    this.tail = (this.tail + 1) % this.queue.length;
+    this.queue[this.tail] = value;
+    this.size ++;
+    return true;
+  }
+
+  deQueue(): boolean {
+    if (this.isEmpty()) return false;
+    if (this.head === this.tail) {
+      this.head = -1;
+      this.tail = -1;
+      this.size --;
+      return true;
+    }
+    this.head = (this.head + 1) % this.queue.length;
+    this.size --;
+    return true;
+  }
+
+  Front(): any {
+    return this.isEmpty() ? -1 : this.queue[this.head];
+  }
+
+  Rear(): any {
+    return this.isEmpty() ? -1 : this.queue[this.tail];
+  }
+
+  isEmpty(): boolean {
+    return !this.size;
+  }
+
+  isFull(): boolean {
+    return this.size === this.queue.length;
+  }
+}
+
+
+class CircularQueue1 {
+  queue: any[];
   head: number = 0;
   tail: number = 0;
   size: number = 0;
