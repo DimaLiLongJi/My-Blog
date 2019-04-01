78bb35c4a7dc362bed7fa7e5489540cf1ce3e559
zyt <zhangxu@gxdgroup.com.cn>
2019-04-01 13:49:59 +0800

    add zone test

---
+++ demo/zone-test.ts
@@ -0,0 +1,24 @@
+import 'zone.js';
+
+Zone.current.fork({
+    name: 'printAsyncTime',
+    onInvokeTask(parentDelegate: ZoneDelegate, currentZone: Zone, targetZone: Zone, task: Task, applyThis: any, applyArgs: any[]) {
+        let 
+        // startTime = performance.now(),
+            result = parentDelegate.invokeTask(targetZone, task, applyThis, applyArgs);
+        // console.log(`${task.source} 耗时 ${performance.now() - startTime}ms `, task.data);
+        return result;
+    }
+}).run(Main)
+
+function Main() {
+    setTimeout(function whenTimeout() {
+        let i = 0;
+        while ((i++) < 999999999) { }
+    });
+
+    // document.addEventListener('click', function whenDocumentClick() {
+    //     let i = 0;
+    //     while ((i++) < 88888888) { }
+    // });
+}
