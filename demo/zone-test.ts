339e3232c1182ce4fffad0267d436b3dcf6df582
zyt <zhangxu@gxdgroup.com.cn>
2019-04-01 15:17:48 +0800

    add zoneJS

--- demo/zone-test.ts
+++ demo/zone-test.ts
@@ -3,10 +3,9 @@ import 'zone.js';
 Zone.current.fork({
     name: 'printAsyncTime',
     onInvokeTask(parentDelegate: ZoneDelegate, currentZone: Zone, targetZone: Zone, task: Task, applyThis: any, applyArgs: any[]) {
-        let 
-        // startTime = performance.now(),
+        let startTime = performance.now(),
             result = parentDelegate.invokeTask(targetZone, task, applyThis, applyArgs);
-        // console.log(`${task.source} 耗时 ${performance.now() - startTime}ms `, task.data);
+        console.log(`${task.source} 耗时 ${performance.now() - startTime}ms `, task.data);
         return result;
     }
 }).run(Main)
@@ -17,8 +16,8 @@ function Main() {
         while ((i++) < 999999999) { }
     });
 
-    // document.addEventListener('click', function whenDocumentClick() {
-    //     let i = 0;
-    //     while ((i++) < 88888888) { }
-    // });
+    document.addEventListener('click', function whenDocumentClick() {
+        let i = 0;
+        while ((i++) < 88888888) { }
+    });
 }
